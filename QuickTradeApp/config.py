a2d93b7f0acf93685c14366ab88e69db1edf4330
henil <henilalagiya@gmail.com>
2025-06-19 19:04:09 -0400

    Simplify config to use environment variables for BASE_URL

--- QuickTradeApp/config.py
+++ QuickTradeApp/config.py
@@ -3,23 +3,8 @@ Configuration settings for QuickTradeApp
 """
 import os
 
-def get_base_url():
-    """Get the current base URL of the application"""
-    try:
-        # Check if we're in production (on Render)
-        if os.environ.get('RENDER'):
-            # Get the service URL from environment
-            service_url = os.environ.get('RENDER_EXTERNAL_URL')
-            if service_url:
-                return service_url
-        # Default to localhost for development
-        return 'http://127.0.0.1:8000'
-    except:
-        # If that fails, use the default
-        return 'http://127.0.0.1:8000'
-
-# Get the base URL
-BASE_URL = get_base_url()
+# Get the base URL from environment variable
+BASE_URL = os.environ.get('BASE_URL', 'http://127.0.0.1:8000')
 
 # API Redirect URLs
 FYERS_REDIRECT_URL = f"{BASE_URL}/fyers/auth/"
